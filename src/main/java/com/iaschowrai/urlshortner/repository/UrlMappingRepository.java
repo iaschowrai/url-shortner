9d70881dd19302bfb41aaec8ba9c7292da38b6d6
iaschowrai <iaschowrai@gmail.com>
2025-03-17 14:22:17 -0400

    URLMapping repository

--- src/main/java/com/iaschowrai/urlshortner/repository/UrlMappingRepository.java
+++ src/main/java/com/iaschowrai/urlshortner/repository/UrlMappingRepository.java
@@ -1,4 +1,16 @@
 package com.iaschowrai.urlshortner.repository;
 
-public interface UrlMappingRepository {
+import com.iaschowrai.urlshortner.models.UrlMapping;
+import com.iaschowrai.urlshortner.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
+
 }
