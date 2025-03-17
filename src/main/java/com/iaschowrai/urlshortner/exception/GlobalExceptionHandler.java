31cdfb6c2473dee90364a8f471c4ebfb0bd8ba0e
iaschowrai <iaschowrai@gmail.com>
2025-03-17 01:53:24 -0400

    Global exception handling are added for login and register

--- src/main/java/com/iaschowrai/urlshortner/exception/GlobalExceptionHandler.java
+++ src/main/java/com/iaschowrai/urlshortner/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.iaschowrai.urlshortner.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
 }
