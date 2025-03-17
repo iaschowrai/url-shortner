ccffc6db8aa8c7edd326ba304b40c672d6c15daa
iaschowrai <iaschowrai@gmail.com>
2025-03-17 14:23:42 -0400

    URLController configured with method level security and to create shorten url and get list of url based on user

--- src/main/java/com/iaschowrai/urlshortner/controller/UrlController.java
+++ src/main/java/com/iaschowrai/urlshortner/controller/UrlController.java
@@ -1,4 +1,44 @@
 package com.iaschowrai.urlshortner.controller;
 
+
+import com.iaschowrai.urlshortner.dtos.UrlMappingDTO;
+import com.iaschowrai.urlshortner.models.User;
+import com.iaschowrai.urlshortner.service.UserService;
+import com.iaschowrai.urlshortner.service.urlservice.UrlMappingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/urls")
 public class UrlController {
+    private final UrlMappingService urlMappingService;
+    private final UserService userService;
+
+    public UrlController(UrlMappingService urlMappingService, UserService userService) {
+        this.urlMappingService = urlMappingService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/shorten")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<UrlMappingDTO> createShortUrl (@RequestBody Map<String, String> request,
+                                                         Principal principal){
+        String originalUrl = request.get("originalUrl");
+        User user = userService.findByUsername(principal.getName());
+        UrlMappingDTO urlMappingDto = urlMappingService.createShortUrl(originalUrl,user);
+        return ResponseEntity.ok(urlMappingDto);
+    }
+
+    @GetMapping("/myUrls")
+    public ResponseEntity<List<UrlMappingDTO>> getUserUrls(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        List<UrlMappingDTO> urls = urlMappingService.getUrlsByUser(user);
+        return ResponseEntity.ok(urls);
+    }
+
 }
