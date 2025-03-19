e0b2808f5180603a09a08f6fed17b9f5e456d199
iaschowrai <iaschowrai@gmail.com>
2025-03-18 22:08:02 -0400

    Click Event DTO modfied

--- src/main/java/com/iaschowrai/urlshortner/dtos/ClickEventDTO.java
+++ src/main/java/com/iaschowrai/urlshortner/dtos/ClickEventDTO.java
@@ -1,4 +1,35 @@
 package com.iaschowrai.urlshortner.dtos;
 
+
+import java.time.LocalDate;
+
+
 public class ClickEventDTO {
+
+    private LocalDate clickDate;
+    private Long count;
+
+    public ClickEventDTO(LocalDate clickDate, Long count) {
+        this.clickDate = clickDate;
+        this.count = count;
+    }
+
+    public ClickEventDTO() {
+    }
+
+    public LocalDate getClickDate() {
+        return clickDate;
+    }
+
+    public void setClickDate(LocalDate clickDate) {
+        this.clickDate = clickDate;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
 }
