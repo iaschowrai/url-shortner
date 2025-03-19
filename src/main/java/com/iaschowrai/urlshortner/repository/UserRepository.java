ce282cadeaf29241dc9942114d2938d4ed0c67f2
iaschowrai <iaschowrai@gmail.com>
2025-03-18 22:12:38 -0400

    User Repository updated

--- src/main/java/com/iaschowrai/urlshortner/repository/UserRepository.java
+++ src/main/java/com/iaschowrai/urlshortner/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.iaschowrai.urlshortner.repository;
 
 import com.iaschowrai.urlshortner.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
