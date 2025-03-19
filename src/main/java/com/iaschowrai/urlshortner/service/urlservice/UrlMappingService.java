8f9fc30852b9d9c3c3a93dc4d97d38e5445064cf
iaschowrai <iaschowrai@gmail.com>
2025-03-18 22:10:02 -0400

    multiple business logic is created for Analytical purpose

--- src/main/java/com/iaschowrai/urlshortner/service/urlservice/UrlMappingService.java
+++ src/main/java/com/iaschowrai/urlshortner/service/urlservice/UrlMappingService.java
@@ -1,26 +1,33 @@
 package com.iaschowrai.urlshortner.service.urlservice;
 
+import com.iaschowrai.urlshortner.dtos.ClickEventDTO;
 import com.iaschowrai.urlshortner.dtos.UrlMappingDTO;
+import com.iaschowrai.urlshortner.models.ClickEvent;
 import com.iaschowrai.urlshortner.models.UrlMapping;
 import com.iaschowrai.urlshortner.models.User;
+import com.iaschowrai.urlshortner.repository.ClickEventRepository;
 import com.iaschowrai.urlshortner.repository.UrlMappingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UrlMappingService {
 
     private static final Logger logger = LoggerFactory.getLogger(UrlMappingService.class);
     private final UrlMappingRepository urlMappingRepository;
-
-    public UrlMappingService(UrlMappingRepository urlMappingRepository) {
+    private final ClickEventRepository clickEventRepository;
+    public UrlMappingService(UrlMappingRepository urlMappingRepository, ClickEventRepository clickEventRepository) {
         this.urlMappingRepository = urlMappingRepository;
+        this.clickEventRepository = clickEventRepository;
     }
 
     public UrlMappingDTO createShortUrl(String originalUrl, User user) {
@@ -90,10 +97,73 @@ public class UrlMappingService {
             throw new IllegalArgumentException("User cannot be null.");
         }
 
-
         return urlMappingRepository.findByUser(user)
                 .stream()
                 .map(this::convertToDto)
                 .toList();
+    }
+
+    public List<ClickEventDTO> getClickEventsByDate(String shortUrl, LocalDateTime start, LocalDateTime end) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping == null){
+            logger.warn("No URL mapping found for short URL: {}", shortUrl);
+            return List.of();
+        }else{
+            return clickEventRepository
+                    .findByUrlMappingAndClickDateBetween(urlMapping,start,end)
+                    .stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    click -> click.getClickDate().toLocalDate()
+                                    ,Collectors.counting()
+                            )
+                    )
+                    .entrySet()
+                    .stream().map(
+                            entry -> {
+                                ClickEventDTO clickEventDTO = new ClickEventDTO();
+                                clickEventDTO.setClickDate(entry.getKey());
+                                clickEventDTO.setCount(entry.getValue());
+                                return clickEventDTO;
+                            }
+                    )
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public Map<LocalDate, Long> getTotalClicksByUserAndDate(User user, LocalDate start, LocalDate end) {
+
+        // Find all URL mappings by the user
+        List<UrlMapping> urlMappings = urlMappingRepository.findByUser(user);
+
+        // Fetch all ClickEvents in the given date range for these URL mappings
+        List<ClickEvent> clickEvents = clickEventRepository.findByUrlMappingInAndClickDateBetween(urlMappings, start.atStartOfDay(), end.plusDays(1).atStartOfDay());
+      return clickEvents
+              .stream()
+              .collect(
+                      Collectors.groupingBy(
+                              click -> click.getClickDate()
+                                      .toLocalDate(), Collectors.counting()
+                      )
+              );
+    }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping == null) {
+            logger.warn("Short URL not found: {}", shortUrl);
+            return null;
+        }else{
+            urlMapping.setClickCount(urlMapping.getClickCount()+1);
+            urlMappingRepository.save(urlMapping);
+
+            // Record Click Events
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+        logger.info("Redirecting to original URL: {} with updated click count: {}", urlMapping.getOriginalUrl(), urlMapping.getClickCount());
+        return urlMapping;
     }
 }
